356ec7c1eaf741d4eb8fa31425add42fc4f2bfe6
Marco Delmastro <marco.delmastro@gmail.com>
2023-12-04 09:59:07 +0100

    Input submodule

---
+++ submodules.sh
@@ -0,0 +1,1 @@
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
